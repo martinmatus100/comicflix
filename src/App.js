28cdcf60c25f9037465976d03ed04574c720e013
martinmatus100 <martinmatus100@gmail.com>
2022-10-17 08:46:19 -0300

    [UPDATE] Change name proyect [Martin]

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Netflix_2015_N_logo.svg/1200px-Netflix_2015_N_logo.svg.png"} className="header__logo" alt="logo"></img>
+        <div className='header__nav'><h3>Marvel</h3><h3>DC</h3><h3>Comics</h3></div>
+        <div className='header__buttons'><button>Carrito</button></div>
       </header>
+      <main className='mainBox'>
+        <div className='card'>
+          <span className='card__name'>Comics Marvel</span>
+          <p className='card__description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim minus suscipit sunt minima repellendus asperiores recusandae ullam hic magnam, voluptates quia cumque architecto adipisci alias laudantium ab. Soluta, reprehenderit. Quisquam?</p>
+          <span className='card__price'>$10.00</span>
+          <span className='card__stock'>Quedan 5</span>
+        </div>
+        <div className='card'>
+          <span className='card__name'>Comics Marvel</span>
+          <p className='card__description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim minus suscipit sunt minima repellendus asperiores recusandae ullam hic magnam, voluptates quia cumque architecto adipisci alias laudantium ab. Soluta, reprehenderit. Quisquam?</p>
+          <span className='card__price'>$10.00</span>
+          <span className='card__stock'>Quedan 5</span>
+        </div>
+        <div className='card'>
+          <span className='card__name'>Comics Marvel</span>
+          <p className='card__description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim minus suscipit sunt minima repellendus asperiores recusandae ullam hic magnam, voluptates quia cumque architecto adipisci alias laudantium ab. Soluta, reprehenderit. Quisquam?</p>
+          <span className='card__price'>$10.00</span>
+          <span className='card__stock'>Quedan 5</span>
+        </div>
+        <div className='card'>
+          <span className='card__name'>Comics Marvel</span>
+          <p className='card__description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim minus suscipit sunt minima repellendus asperiores recusandae ullam hic magnam, voluptates quia cumque architecto adipisci alias laudantium ab. Soluta, reprehenderit. Quisquam?</p>
+          <span className='card__price'>$10.00</span>
+          <span className='card__stock'>Quedan 5</span>
+        </div>
+      </main>
+      <footer className='footer'>
+        <h3 className='footer__item'>Marvel</h3>
+        <h3 className='footer__item'>DC</h3>
+        <h3 className='footer__item'>Comics</h3>
+      </footer>
     </div>
   );
 }
